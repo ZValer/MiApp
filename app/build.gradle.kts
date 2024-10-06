5d9212954ade8542e5a135ba0c3206b5bf84cb69
Valeria Zuñiga Mendoza <A01705435@tec.mx>
2024-10-06 12:39:38 -0600

    chore: Añadir dependencias en el gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,14 +33,39 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //Retrofit is used for network calls, with a Gson converter to parse JSON responses from a web API into Java/Kotlin objects
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Glide libraries
+    // Glide library for image loading and caching
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    // Glide transformations for applying effects to images
+    implementation("jp.wasabeef:glide-transformations:4.3.0")
+
+    // Kotlin Coroutines for handling asynchronous tasks
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+
+    //implementation("androidx.fragment:fragment-ktx:1.5.9")
+    implementation("androidx.activity:activity-ktx:1.7.0")
+
+    implementation("androidx.databinding:databinding-runtime:7.1.2")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
 }
